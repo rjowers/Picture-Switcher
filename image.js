98b25f92b96b85267de587783c202dd1e3c40b9c
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-08 13:10:06 -0500

    setup

---
+++ image.js
@@ -0,0 +1,28 @@
+
+var carouselImages = [
+  {
+    image_url: 'http://desktopography.net/wp-content/uploads/bfi_thumb/hushed_1920x1200-32f5431dh4se5dwk02ct1c.jpg',
+    title: 'Hushed',
+    photographer_id: 'Michael Withers'
+  },
+  {
+    image_url: 'http://desktopography.net/wp-content/uploads/bfi_thumb/dndln_1920x1200-32f56720k59biqi0157i0w.jpg',
+    title: 'Dndln',
+    photographer_id: 'Carlos'
+  },
+  {
+    image_url: 'http://desktopography.net/wp-content/uploads/bfi_thumb/open_source_1440x900-1-32awggysvbuw444jyfikn4.jpg',
+    title: 'Open Source',
+    photographer_id: 'Jerico Santander'
+  },
+  {
+    image_url: 'http://desktopography.net/wp-content/uploads/bfi_thumb/treehugger_1680x1050-32awgq9j4zatrpd2gj73eo.jpg',
+    title: 'Treehugger',
+    photographer_id: 'Pete Harrison'
+  },
+  {
+    image_url: 'http://desktopography.net/wp-content/uploads/bfi_thumb/dsn_1440x900-32f5578uqrcngwrj99imm8.jpg',
+    title: 'DSN',
+    photographer_id: 'Marius Bauer'
+  }
+];
